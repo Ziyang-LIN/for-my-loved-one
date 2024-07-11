8d56213dd018361303fd9bbacb6c49426959a6fe
Ziyang-LIN <67027249+Ziyang-LIN@users.noreply.github.com>
2024-07-10 22:34:50 -0400

    Added JavaScript for image slider, added font robustness

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+
+function moveSlides(n) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    currentSlide += n;
+
+    if (currentSlide >= totalSlides) {
+        currentSlide = 0;
+    } else if (currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+document.querySelector('.prev').addEventListener('click', () => moveSlides(-1));
+document.querySelector('.next').addEventListener('click', () => moveSlides(1));
