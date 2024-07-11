1958bf8e5ef33ff459d0b3289e9c5999b03fe080
Ziyang-LIN <67027249+Ziyang-LIN@users.noreply.github.com>
2024-07-11 01:48:55 -0400

    Added slider styling and title styling, changed theme.

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ let currentSlide = 0;
 function moveSlides(n) {
     const slides = document.querySelector('.slides');
     const totalSlides = document.querySelectorAll('.slide').length;
-    currentSlide += n;
-
-    if (currentSlide >= totalSlides) {
-        currentSlide = 0;
-    } else if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
-    }
+    currentSlide = (currentSlide + n + totalSlides) % totalSlides;
 
     slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
